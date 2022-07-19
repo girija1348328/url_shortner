ff56738565f509a2947a83f5bb05ad279a58f0d7
Latha-AJ <lathajappa.7226@gmail.com>
2022-07-19 12:32:04 +0530

    changes

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -7,7 +7,6 @@ const baseUrl = 'http://localhost:3000'
 const urlShortner= async function(req,res){
     try{
         let{longUrl}=req.body;
-        // if(!Object.keys().length == 0) return res.status(400).send({status:false,message:"Please enter input"});
         if(!longUrl) return res.status(400).send({status:false,message:"please enter the url"});
         if (!validUrl.isUri(baseUrl)) {
             return res.status(401).send({status:false,message:"Invalid base URL"})
@@ -15,18 +14,23 @@ const urlShortner= async function(req,res){
         const urlCode = shortid.generate()
         if (validUrl.isUri(longUrl)) {
             let url = await urlModel.findOne({longUrl:longUrl})
-            
-            if (url){
-                return res.status(200).send({status:true,data:url})
+            if (url){  
+             let obj = url.toObject();
+              delete obj._id;
+               delete obj.__v;   
+              return res.status(200).send({status:true,data:obj})
             }else{
                 const shortUrl = baseUrl + '/' + urlCode
+               
                 url = new urlModel({
                     longUrl,
                     shortUrl,
                     urlCode
                 })
+                
                 await url.save()
-                return res.status(200).send({status:true,data:url})
+                const data = await urlModel.findById({_id:url._id}).select({__v:0, _id:0})
+                return res.status(200).send({status:true,data:data})
             }
 
         }else{
@@ -43,12 +47,11 @@ module.exports.urlShortner=urlShortner;
 const getUrl= async function(req,res){
     try{
         const urlCode = req.params.urlCode;
-        // if(!urlCode)  return res.status(400).send({status:false,message:"please enter the url"});
         const isUrl= await urlModel.findOne({urlCode:urlCode})
         console.log(isUrl)
         if(!isUrl) return res.status(400).send({status:false,message:"url not found"});
         if(isUrl) {
-            return res.redirect(isUrl.longUrl)
+            return res.status(302).redirect(isUrl.longUrl)
             
         }else{
             return res.status(404).send({status:false,message:"url not found"});
